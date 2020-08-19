a509e3a3361ae6ea1f3fccf5e0f09aad7401f12c
Boaardz56 <jackie.lopez.56@gmail.com>
2020-08-19 13:37:17 -0400

    fixed axios calls to show on console

--- client/src/components/Api.js
+++ client/src/components/Api.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect,useCallback } from 'react'
 import { Router, Route, Switch } from "react-router-dom";
 import history from "../history";
 import { useDispatch } from "react-redux";
@@ -11,7 +11,7 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import PrivateRoute from "./PrivateRoute";
 import NoMatch from "../pages/NoMatch";
-import axios from "axios"
+import axios from "axios";
 import SearchForm from "./SearchForm";
 
 function Api() {
@@ -33,8 +33,8 @@ function Api() {
     setInputValue({ search: event.target.value })
   };
 
-  useEffect(async () => {
-    var configGetToken = {
+  const fetchData = useCallback(() => { 
+    axios({
       //get token
       method: 'post',
       url: 'https://accounts.spotify.com/api/token?grant_type=client_credentials',
@@ -44,15 +44,12 @@ function Api() {
         'Authorization': 'Basic ZmM5NzkyYzNkOWU0NGRmODk1NjM4OTY1YmZmOGI4MGI6NmE2OTYwODNkNDMwNDM5ZGJmMGNmNTRjZDg2MTYyMGY=',
       },
       data: ""
-    };
-
-    axios.get(configGetToken)
-      //got token
-      .then(res => {
+    })
+      .then((res) => {
         var token = res.data.access_token
         console.log(token);
         //another axios get to search with the token(BQBJZeaaEsHYfSeM7ndR7uDcA2IyenVdLq-q9uFHp2V_CTdVl2NQdyGuxG0TdQy0H9cYGR0ntu-yYw7bh04)
-        var configGotToken = {
+        axios({
           method: 'get',
           url: 'https://api.spotify.com/v1/search?q=Greenday&type=artist',
           headers: {
@@ -61,43 +58,43 @@ function Api() {
             'Authorization': ('Bearer ' + token)
           },
           data: ""
-        };
-
-        axios.get(configGotToken)
-          .then(res => {
+        })
+          .then((res) => {
             //set spotify results to get results for table
             setSpotifyResults(res.data);
             setSearch(true);
             console.log(res.data)
           })
-          .catch(function (error) {
+          .catch((error) => {
             console.log(error);
-          });
-      })
-      .catch(function (error) {
-        console.log(error);
+          })
       });
-    //weather API Call
-    if ("geolocation" in navigator) {
+      //weather API Call
+     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
         let lat = position.coords.latitude
         let lon = position.coords.longitude
         console.log("Latitude is :", lat);
         console.log("Longitude is :", lon);
 
-        axios.get({
+        axios({
           method: 'get',
           url: "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=e971f7deaf07913de154d7e7ed5455c5&units=imperial",
-        }).then(res => {
+        })
+        .then((res) => {
           console.log(res.data)
           setWeatherResults(res.data)
         })
-          .catch(function (error) {
+        .catch((error) => {
             console.log(error);
-          });
+        })
       })
     }
-  });
+  }, []);
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
 
   return (
     <div>
@@ -112,7 +109,6 @@ function Api() {
         /> */}
     </div>
   )
-
 };
 
 export default Api; 
